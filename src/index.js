349cd8084313c7a82ef61df8b26edb3382663fd6
Ajay <ajaykcoder@gmail.com>
2024-03-05 23:20:58 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Todo from "./Todo";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Todo/>
+    </React.StrictMode>
+);
