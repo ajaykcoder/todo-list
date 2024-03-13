97fbb1c8f0677dfee20c762f72544beb3fbb2c23
Ajay <ajaykcoder@gmail.com>
2024-03-13 16:08:24 +0530

    Add css

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -40,49 +40,50 @@ const Todo = () => {
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="">Title</label><br/>
-                    <input type="text" name="title" value={form.title} onChange={onChange}/>
-                    {errors.title && (
-                        <div style={{ color: "red", fontSize: "14px" }}>{errors.title}</div>
-                    )}
-                </div>
-                <br/>
-                <div>
-                    <label htmlFor="">Description</label><br/>
-                    <input type="text" name="description" value={form.description} onChange={onChange}/>
-                    {errors.description && (
-                        <div style={{ color: "red", fontSize: "14px" }}>{errors.description}</div>
-                    )}
-                    <br/><br/>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-            <br/><br/><br/>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Title</td>
-                        <td>Description</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {todoList.length > 0 ? (
-                        todoList.map((todo, index) => (
-                            <tr key={index}>
-                                <td>{todo.title}</td>
-                                <td>{todo.description}</td>
+        <div className="flex items-center justify-center h-lvh p-10">
+            <div className="bg-teal-300 p-16 rounded-3xl size-full flex">
+                <form onSubmit={handleSubmit} className="flex-1">
+                    <div>
+                        <label htmlFor="">Title</label><br/>
+                        <input type="text" name="title" value={form.title} onChange={onChange}/>
+                        {errors.title && (
+                            <div style={{ color: "red", fontSize: "14px" }}>{errors.title}</div>
+                        )}
+                    </div>
+                    <div>
+                        <label htmlFor="">Description</label><br/>
+                        <input type="text" name="description" value={form.description} onChange={onChange}/>
+                        {errors.description && (
+                            <div style={{ color: "red", fontSize: "14px" }}>{errors.description}</div>
+                        )}
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+                <div className="w-3/5">
+                    <table className="w-full">
+                        <thead>
+                            <tr>
+                                <td>Title</td>
+                                <td>Description</td>
                             </tr>
-                        ))
-                    ) : (
-                        <tr>
-                            <td colSpan={2}>No Data Found</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+                        </thead>
+                        <tbody>
+                            {todoList.length > 0 ? (
+                                todoList.map((todo, index) => (
+                                    <tr key={index}>
+                                        <td>{todo.title}</td>
+                                        <td>{todo.description}</td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan={2}>No Data Found</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
         </div>
     );
