e9d91dd84fd9d3a6b4db52b86353585eb88973d9
Ajay <ajaykcoder@gmail.com>
2024-04-21 00:58:25 +0530

    Remove json server

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        colors: {
+            "green" : "#4EA818",
+            "darkGreen" : "#4B8F22",
+            "white" : "#FFFFFF",
+            "text" : "#475056",
+            "black" : "#212529",
+            "darkBlack" : "#000000",
+            "red" : "#B00020",
+            "transparent" : "#FFFFFF00"
+        },
+        screens: {
+            "full": {"max": "1580px"},
+            "desktop": {"max": "1440px"},
+            "laptop-lg": {"max": "1280px"},
+            "laptop": {"max": "1199px"},
+            "tablet-lg": {"max": "1024px"},
+            "tablet": {"max": "991px"},
+            "mobile-lg": {"max": "767px"},
+            "mobile": {"max": "479px"},
+        },
+        extend: {
+            fontFamily: {
+                Roboto: ["Roboto", "sans-serif"]
+            }
+        }
     },
     plugins: [],
 }
